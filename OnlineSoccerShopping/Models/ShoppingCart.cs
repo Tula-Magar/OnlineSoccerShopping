a62b40737299c856d8ef59eb999516b6f76080cf
tmagar <tmagar448@gmail.com>
2023-03-15 21:21:54 -0400

    shopping cart model

---
+++ OnlineSoccerShopping/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineSoccerShopping.Models
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public UserAccount User { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+ 
+    }
+}
